c49036a528e200f4a9a786456c96aedc396ebdda
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-09-08 00:23:23 +1000

    Prepare to use AJAX form submission.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,34 +2,24 @@
 module Handler.Home where
 
 import Import
+import Text.Julius (rawJS)
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
     (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
+    let formId = "theForm" :: Text
     defaultLayout $ do
         setTitle "floating-castle"
         $(widgetFile "homepage")
 
-postHomeR :: Handler Html
+postHomeR :: Handler Value
 postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
+    ((result, _), _) <- runFormPost sampleForm
+    let submission = case result of
             FormSuccess res -> Just res
             _ -> Nothing
 
-    defaultLayout $ do
-        setTitle "floating-castle"
-        $(widgetFile "homepage")
+    jsonOrRedirect HomeR (object $ ["value" .= show submission])
 
 sampleForm :: Form Text
 sampleForm = renderDivs $ areq textField "Add an item:" Nothing
