e01c571f91ac3860e161ee018fee6850798b2b59
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-09-11 09:00:30 +1000

    Fixed MongoDB URI parsing.

--- Helpers/Heroku.hs
+++ Helpers/Heroku.hs
@@ -3,11 +3,9 @@
 module Helpers.Heroku (herokuConf) where
 
 import Prelude
-import Data.Text (Text, pack, breakOn)
+import Data.Text (Text, pack, unpack, breakOn)
 import Database.Persist.MongoDB (MongoConf(..), master, MongoAuth(..))
-import Data.Maybe (fromJust)
 import Network (PortID(PortNumber))
-import Network.Socket (PortNumber(PortNum))
 import Data.Word (Word16)
 import Network.URI
 import System.Environment
@@ -18,11 +16,14 @@ herokuConf :: IO MongoConf
 herokuConf = do
     params <- dbConnParams
     let
-        dbname = fromJust $ lookup "dbname" params
-        host = fromJust $ lookup "host" params
-        port = PortNumber $ PortNum (read $ T.unpack (fromJust $ lookup "port" params) :: Word16)
-        username = fromJust $ lookup "user" params
-        password = fromJust $ lookup "password" params
+        getParam n = case lookup n params of
+            Just v -> v
+            Nothing -> error $ "Could not find parameter " ++ unpack n ++ " in database config URL."
+        dbname = getParam "dbname"
+        host = getParam "host"
+        port = PortNumber $ fromIntegral (read $ T.unpack (getParam "port") :: Word16)
+        username = getParam "user"
+        password = getParam "password"
      in return MongoConf {
         mgDatabase = dbname,
         mgHost = host,
@@ -35,35 +36,31 @@ herokuConf = do
      }
 
 dbConnParams :: IO [(Text, Text)]
-dbConnParams = getEnv "MONGOHQ_URL" >>= return . parseDatabaseUrl
+dbConnParams = getEnv "MONGOLAB_URI" >>= return . parseDatabaseUrl
 
 parseDatabaseUrl :: String -> [(Text, Text)]
 parseDatabaseUrl durl =
-  let muri = parseAbsoluteURI durl
-      (auth, dbpath) = case muri of
-                      Nothing ->  error "couldn't parse absolute uri"
-                      Just uri -> if uriScheme uri /= "mongodb:"
-                                    then schemeError uri
-                                    else case uriAuthority uri of
-                                           Nothing   -> invalid
-                                           Just a -> (a, uriPath uri)
-      (user,password) = userAndPassword auth
-  in     [
-          (pack "user",     user)
-          -- tail not safe, but should be there on Heroku
+    let muri = parseAbsoluteURI durl
+        (auth, dbpath) = case muri of
+            Nothing ->  error "couldn't parse absolute uri"
+            Just uri -> if uriScheme uri /= "mongodb:"
+                then schemeError uri
+                else case uriAuthority uri of
+                    Nothing   -> invalid
+                    Just a -> (a, uriPath uri)
+        (user,password) = userAndPassword auth
+     in
+        -- Prelude.tail is unsafe but these settings should always exist.
+         [(pack "user",     user)
          ,(pack "password", T.tail password)
          ,(pack "host",     pack $ uriRegName auth)
-         -- Heroku should use default port
-         -- ,(pack "port",     pack $ uriPort auth)
-         -- tail not safe but path should always be there
+         ,(pack "port",     pack $ Prelude.tail $ uriPort auth)
          ,(pack "dbname",   pack $ Prelude.tail $ dbpath)
          ]
   where
-    -- init is not safe, but should be there on Heroku
+    -- Prelude.init is not safe, but should be there on Heroku
     userAndPassword :: URIAuth -> (Text, Text)
     userAndPassword = (breakOn $ pack ":") . pack . Prelude.init . uriUserInfo
-
-    schemeError uri = error $ "was expecting a postgres scheme, not: " ++ (uriScheme uri) ++ "\n" ++ (show uri)
-    -- should be an error 
-    invalid = error "could not parse heroku DATABASE_URL"
+    schemeError uri = error $ "was expecting a mongodb scheme, not: " ++ (uriScheme uri) ++ "\n" ++ (show uri)
+    invalid = error "could not parse heroku MONGOLAB_URI"
 
